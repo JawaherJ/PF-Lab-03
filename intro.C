8881c880ae4b5338dcd377d6dc6c37677757ef01
BismaJabbar <bismajabbar1@gmail.com>
2026-02-07 01:48:43 -0800

    Task 9: Print double with default, 2 decimal, and 5 decimal precision

--- intro.C
+++ intro.C
@@ -1,27 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int num;
-    float fnum;
-    char ch;
+    double num;
 
    
-    printf("Enter an integer: ");
-    scanf("%d", &num);
+    printf("Enter a double value: ");
+    scanf("%lf", &num);
 
- 
-    printf("Enter a float: ");
-    scanf("%f", &fnum);
-
-  
-    printf("Enter a character: ");
-    scanf(" %c", &ch);  
-
-  
-    printf("\nYou entered:\n");
-    printf("Integer: %d\n", num);
-    printf("Float: %.2f\n", fnum);
-    printf("Character: %c\n", ch);
+    
+    printf("\nYou entered: %lf (default precision)\n", num);
+    printf("2 decimal places: %.2lf\n", num);
+    printf("5 decimal places: %.5lf\n", num);
 
     return 0;
 }
